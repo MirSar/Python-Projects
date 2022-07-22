9397f2f3df2420d5178d307f97557b3f2968b14a
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-07-22 16:18:55 -0700

    file cleanup changes
    reorganized files and folders

---
+++ python_Projects/File Transfer App/file_transfer.py
@@ -0,0 +1,43 @@
+#===================================================================================
+#===================================================================================
+
+# Python:       3.10.5
+
+# Author:       Mirwais Sarwary
+
+# Purpose:      The Tech Academy - Python Course
+
+# Assignment:   File Transfer CHALLENGE
+"""
+Your employer wants a program developed to move files from one folder
+to another with the click of a button.
+
+Complete these actions:
+    1.  Create a GUI. This GUI will act as the interface for users
+        to transfer files from one directory to another. 
+
+"""
+
+#===================================================================================
+#================
+# Import Section:
+#================
+
+#=============
+#Main Section:
+#=============
+
+
+
+
+        
+#===================================================================================
+#program flow control:
+#=====================
+#looks at this first to see which script, if any, to run first
+
+if __name__ == "__main__":
+    pass    #name of function or pass
+    
+#===================================================================================
+#===================================================================================
